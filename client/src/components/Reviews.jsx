4c94acdf75948eae0f18cae52ea4fed662053d88
Dustin Jurkaulionis <dustinjurkaulionis@gmail.com>
2019-06-29 16:20:36 -0400

    reviews dyamically showing with local state

--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 class Reviews extends Component {
 	state = {
+		name: "",
+		score: "",
+		review: "",
 		reviewList: [
 			{ name: "dustin", score: 5, review: "dece" },
 			{ name: "vlad", score: 3, review: "ye" }
@@ -9,7 +12,7 @@ class Reviews extends Component {
 
 	renderReviewsTable = () => {
 		if (this.state.reviewList.length > 0) {
-			return this.state.reviewList.map(this.renderReviewFormat);
+			return this.state.reviewList.map(this.populateReviewTable);
 		} else {
 			return (
 				<tr>
@@ -24,7 +27,23 @@ class Reviews extends Component {
 		}
 	};
 
-	renderReviewFormat = review => (
+	onSubmit = e => {
+		e.preventDefault();
+		//console.log(this.state.reqData);i
+		//const errors = this.validate(this.state.reqData);
+		//if (errors.length === 0) {
+		//this.setState({ ...this.state, formErrors: [] });
+		this.setState(prevState => ({
+			reviewList: [...prevState.reviewList, this.state]
+		}));
+		//this.props.fetchParks(this.state.reqData);
+		// } else {
+		// 	this.setState({ ...this.state, formErrors: errors });
+		// }
+		//getparks(reqdata) of parent
+	};
+
+	populateReviewTable = review => (
 		<tr>
 			<td>{review.name}</td>
 			<td>{review.score}</td>
@@ -32,17 +51,85 @@ class Reviews extends Component {
 		</tr>
 	);
 
+	//handle any form input change at once
+	//onChange={this.handleChange.bind(this)} for each input
+	// handleChange = e => {
+	// 	// If you are using babel, you can use ES 6 dictionary syntax
+	// 	// let change = { [e.target.name] = e.target.value }
+	// 	let change = {};
+	// 	change[e.target.name] = e.target.value;
+	// 	this.setState(change);
+	// };
+
+	handleNameChange = e => {
+		this.setState(e.target.value);
+	};
+	handleScoreChange = e => {
+		this.setState(e.target.value);
+	};
+	handleReviewChange = e => {
+		this.setState(e.target.value);
+	};
+
 	render() {
 		return (
-			<div className="border border-primary">
-				<table className="table table-hover">
-					<tr>
-						<th>Name</th>
-						<th>Score</th>
-						<th>Review</th>
-					</tr>
-					<tbody>{this.renderReviewsTable()}</tbody>
-				</table>
+			<div>
+				<form id="reviewForm">
+					<label>
+						Name: <br />
+						<input
+							type="text"
+							placeholder="Name"
+							name="name"
+							onChange={this.handleNameChange}
+							value={this.state.name}
+						/>
+					</label>
+					<br />
+					<label>
+						Score (1-5):
+						<br />
+						<input
+							type="number"
+							min="1"
+							max="5"
+							name="score"
+							onChange={this.handleScoreChange}
+							value={this.state.score}
+						/>
+					</label>
+					<br />
+					Review
+					<br />
+					<label>
+						<textarea
+							rows="4"
+							cols="30"
+							name="review"
+							placeholder="Enter text here..."
+							onChange={this.handleReviewChange}
+							value={this.state.review}
+						/>
+					</label>
+					<br />
+					<button
+						className="btn btn-primary m-2"
+						onClick={e => this.onSubmit(e)}
+					>
+						Submit
+					</button>
+				</form>
+
+				<div className="border border-primary">
+					<table className="table table-hover">
+						<tr>
+							<th>Name</th>
+							<th>Score</th>
+							<th>Review</th>
+						</tr>
+						<tbody>{this.renderReviewsTable()}</tbody>
+					</table>
+				</div>
 			</div>
 		);
 	}
