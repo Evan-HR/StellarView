d99d133bf32d3db9a3eeb57d27f16f1f8aead7ab
CyberTropic <CyberTropic@users.noreply.github.com>
2019-07-02 23:01:45 -0400

    Working on the UNKNOWNS

--- qgis/park_name_generator.py
+++ qgis/park_name_generator.py
@@ -8,7 +8,7 @@ import requests
 with open('../data/ontario_parks.csv', newline='') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     csv_writer = csv.writer(
-        open('../data/ontario_parks_out.csv', 'w', newline=''))
+        open('../data/ontario_parks_out.csv', 'w', newline='', encoding='utf-8'))
     line_count = 0
     csv_writer.writerow(
         ["id", "osmid", "name", "alt_name", "light_pol", "lat", "lng"])
@@ -20,7 +20,17 @@ with open('../data/ontario_parks.csv', newline='') as csv_file:
             response = requests.get(
                 f'https://nominatim.openstreetmap.org/reverse?format=json&lat={row[5]}&lon={row[4]}')
             data = response.json()
-            print(f'{data["address"][0]} {data["address"][1]}')
+            if 'name' in data.keys():
+                print(data['name'])
+            else:
+                if 'house_number' in data['address'].keys() and data['address']['house_number'] == data['display_name'].split(', ')[0]:
+                    alt_name = f"{data['display_name'].split(', ')[0]} {data['display_name'].split(', ')[1]}"
+                    print(
+                        f"{data['display_name'].split(', ')[0]} {data['display_name'].split(', ')[1]}")
+                else:
+                    alt_name = f"{data['display_name'].split(', ')[0]}"
+                    print(
+                        f"{data['display_name'].split(', ')[0]}")
         csv_writer.writerow(
-            [row[0], row[1], row[2], alt_name, row[3], row[4], row[5]])
+            [row[0], row[1], row[2], alt_name, row[3], row[5], row[4]])
     print(f'Processed {line_count} unknowns.')
