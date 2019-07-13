1dfe80ee5d6012b8f1938d144dc8c221cff52b7d
CyberTropic <vfalach@gmail.com>
2019-07-13 01:39:34 -0400

    Nice python formatting

--- qgis/mergeCSV.py
+++ qgis/mergeCSV.py
@@ -1,17 +1,17 @@
-#Simple csv merge script
+# Simple csv merge script
 import os
 
-fout=open("merged_data.csv","w")
+fout = open("merged_data.csv", "w")
 first = True
 for file in os.listdir("."):
-    if file.endswith(".csv") and file != "merged_data.csv": 
+    if file.endswith(".csv") and file != "merged_data.csv":
         print(file)
         f = open(file, "r")
-        if (first):
+        if first:
             first = False
         else:
-            f.readline() # skip the header
+            f.readline()  # skip the header
         for line in f:
             fout.write(line)
-        f.close() # not really needed
+        f.close()  # not really needed
 fout.close()
