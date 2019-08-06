e00ea9f6a58b51ce6cd46ed7dce43421b002dc32
CyberTropic <vfalach@gmail.com>
2019-08-05 22:24:47 -0400

    Fixed merge error

--- client/src/components/ParkMap.jsx
+++ client/src/components/ParkMap.jsx
@@ -250,11 +250,7 @@ class ParkMap extends Component {
 				<div
 					ref={this.googleMapRef}
 					className="border border-primary"
-<<<<<<< HEAD
 					style={{ width: "100%", height: "100%", zIndex:"-1"}}
-=======
-					style={{ width: "100%", height: "100%" }}
->>>>>>> 14730b7e9268cd595bf9693f2f578adddfa110fa
 				/>
 				{/* <div>
 					<button onClick={this.centerMap}>Re-center</button>
