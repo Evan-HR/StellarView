b1faa16491d3d94097f479f3457b73449a1d4ac7
CyberTropic <CyberTropic@users.noreply.github.com>
2019-08-26 15:16:56 -0400

    Logo z-index + css cleanup

--- client/src/components/ToolBar/Toolbar.js
+++ client/src/components/ToolBar/Toolbar.js
@@ -102,30 +102,24 @@ export default withRouter(Toolbar);
 ////////////////////////////////////////////////
 
 const ToolbarStyle = styled.header`
-
-
 	width: 100%;
 	/* background: ${props => props.theme.darkBlue3}; */
-	
 	height: 120px;
 	top: 0;
 	left: 0;
-	z-index: 100;
+	/* z-index: 100; */
 	
-	@media screen and (max-width: 767.999px){
+	@media screen and (max-width: 767.999px) {
 		position: absolute;
-		}
-
-
+	}
 
 	.toolbar__center{
 		display: block;
 		width:85%;
 		height: 100%;
 		margin: 0 auto 0 auto;
-		
-		@media screen and (max-width: 767.999px){
-		width: 95%;
+		@media screen and (max-width: 767.999px) {
+			width: 95%;
 		}
 	}
 
@@ -134,10 +128,7 @@ const ToolbarStyle = styled.header`
 		align-items: center;
 		height: 100%;
 		bottom: 0;
-
 		/* border-bottom: 2px solid #7C6E7E; */
-
-		
 	}
 
 	.toolbar_navigation-items{
@@ -158,52 +149,37 @@ const ToolbarStyle = styled.header`
 		text-transform: uppercase;
 		list-style: none;
 		padding: 0;
-		display: flex;
-		
+		display: flex;	
 	}
 
-
 	.toolbar_navigation-items li {
-		
 		padding: 0 0.5rem;
 	}
 
-	.toolbar_navigation-items li:last-child {padding: 0 0 0 0.5rem;}
-	/* On screens that are 768px or less*/
+	.toolbar_navigation-items li:last-child {
+		padding: 0 0 0 0.5rem;
+	}
 
+	/* On screens that are 768px or less*/
 	@media screen and (max-width: 767.999px) {
 		.toolbar_navigation-items {
 			display: none;
 			height: 60px;
-			
-			
 		}
-		
 		height: 60px;
 		.toolbar__logo{
 			padding-top: 10px;
 			a{
 				font-size: 50px;
 			}
-			
-
 		}
-
-
-
-
 	}
 
 	@media screen and (min-width: 769px) and (max-width: 1300px) {
 		.toolbar__center{
-		width: 90%;
+			width: 90%;
 		}
-
-
-		
 	}
-
-	
 
 	.spacer {
 		flex: 1;
@@ -214,79 +190,56 @@ const ToolbarStyle = styled.header`
 		/* padding-bottom: 2.05em;
 		border-bottom: 3px solid ${props => props.theme.gold}; */
 		transition: color 0.2s ease;
-  		color: ${props => props.theme.colorBad};
-		
+  		color: ${props => props.theme.colorBad};		
 	}
 
-	.toolbar__logo{
-	
+	.toolbar__logo{	
 		font-weight: 400;
 		font-size: 60px;
-font-family: IBM Plex Sans;
-.Stellar{
-	font-weight: 600;
-}
-.Gaze{
-	font-weight: 300;
-}
-  /* text-transform: uppercase;
-  font-weight: 400;
-  font-size: 70px;
-  text-shadow: 4px 5px #e6e6d8, 6px 7px #c6a39a; */
-  /* transform: matrix(1, -0.05, 0, 1, 0, 0); */
-  
+		font-family: IBM Plex Sans;
+		.Stellar{
+			font-weight: 600;
+		}
+		.Gaze{
+			font-weight: 300;
+		}
+		/* text-transform: uppercase;
+		font-weight: 400;
+		font-size: 70px;
+		text-shadow: 4px 5px #e6e6d8, 6px 7px #c6a39a; */
+		/* transform: matrix(1, -0.05, 0, 1, 0, 0); */
+		:hover{
+			.logo_A {
+				transition: color 1.0s ease;
+				color: ${props => props.theme.gold2};
+			}
+			.logo_tele{		
+				transform: rotate(-5deg);
+				transition: transform 1s;
+			}
+		}
 
-
-:hover{
-
-		.logo_A {
+		.logo_A{
 			transition: color 1.0s ease;
-  color: ${props => props.theme.gold2};
+			color: ${props => props.theme.franRed};
 		}
 
 		.logo_tele{
-			
-			transform: rotate(-5deg);
-			
-			transition: transform 1s;
-		}
-	}
-
-	.logo_A{
-		transition: color 1.0s ease;
-  color: ${props => props.theme.franRed};
-		
-	}
-
-	.logo_tele{
-		/* transform: translate(-505%,-120%); */
-					transform: rotate(5deg);
-			
+			/* transform: translate(-505%,-120%); */
+			transform: rotate(5deg);
 			transition: transform 1s;
 			position: absolute;
 			margin-left: -121px;
-    margin-top: -10px;
-  
-		
-	}
-
-
+			margin-top: -10px;			
+		}
 	}
 
 	.Link{
 		text-decoration:none;
-	
 	}
 
 	.toolbar__logo a{
 		text-decoration: none;
 		color: whitesmoke;
 	}
-
-
-
-
-
-
-
 `;
