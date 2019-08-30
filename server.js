ca57340446a90899147ac2c9cc43360e3a54ec60
Dustin Jurkaulionis <dustinjurkaulionis@gmail.com>
2019-08-30 15:45:44 -0400

    blah

--- server.js
+++ server.js
@@ -36,9 +36,12 @@ const port = process.env.PORT || 5000;
 //for dynamic html generation
 app.set("view engine", "ejs");
 //Serving css
-app.use("/public", express.static("public"));
+app.use(express.static(path.join(__dirname, "client", "build")));
 //app.use(express.static(__dirname + '/public'));
 
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 //arbitrary port 5000
 app.listen(port, () => {
 	console.log(`Server started on port ${port}`);
