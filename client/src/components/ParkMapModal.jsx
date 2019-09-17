4fc6bac63f05537560aff075fd2715a3359b8523
Dustin Jurkaulionis <dustinjurkaulionis@gmail.com>
2019-09-17 18:00:56 -0400

    brief modal header fix

--- client/src/components/ParkMapModal.jsx
+++ client/src/components/ParkMapModal.jsx
@@ -407,7 +407,8 @@ const ModalStyle = styled.div`
 		font-weight: normal;
 		color: ${props => props.theme.white};
 		font-size: 30px;
-		padding: 2rem 1rem;
+		text-align: left;
+		padding: 1rem 2.5rem 2rem 1rem;
 		background: ${props => props.theme.mapBlue};
 		border: none;
 		border-radius: 0rem;
