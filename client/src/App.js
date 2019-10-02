358fc335eee7c6de586124cab0efa151c56f7db6
Dustin Jurkaulionis <dustinjurkaulionis@gmail.com>
2019-10-01 22:50:31 -0400

    wut

--- client/src/App.js
+++ client/src/App.js
@@ -59,12 +59,12 @@ class App extends Component {
 		return (
 			<React.Fragment>
 				<Helmet>
-					{/* <meta property="og:image" content={SocialLogo} />
+					<meta property="og:image" content={SocialLogo} />
 					<meta property="og:image:height" content="341" />
-					<meta property="og:image:width" content="650" /> */}
+					<meta property="og:image:width" content="650" />
 					<meta
 						property="og:description"
-						content="Quickly find areas to stargaze near you."
+						content="Quickly find nearby areas to stargaze."
 					/>
 					<meta property="og:title" content="STELLARGAZE" />
 					<meta property="og:url" content="www.stellargaze.com" />
