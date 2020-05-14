ad0622528e54e4a4067dce4b468bc38199d1ad57
CyberTropic <CyberTropic@users.noreply.github.com>
2020-05-14 15:22:59 -0400

    fixed AGAIN It was the slash direction lol

--- server/server.js
+++ server/server.js
@@ -911,6 +911,7 @@ app.post("/api/getParkData", async (req, res) => {
 	const queryFromUserForm =
 		"SELECT *, ( 6371 * acos( cos( radians( ? ) ) * cos( radians( lat ) ) * cos( radians( lng ) - radians( ? ) ) + sin( radians( ? ) ) * sin( radians( lat ) ) ) ) AS distance FROM ontario_parks HAVING distance <= ? AND light_pol <= ? ORDER BY distance ASC";
 
+	//TODO: see if this can be even more elegant
 	try {
 		var initialResults = await new Promise((resolve, reject) => {
 			connection.query(
@@ -977,91 +978,149 @@ app.post("/api/getParkData", async (req, res) => {
 	const allReviewsQuery = `select AVG(score)as avgScore,count(*) as numReviews,p_id from reviews where p_id in ${inParkIDSet} group by p_id`;
 	if (reviewIDsOriginal && reviewIDsOriginal.length) {
 		console.log("NEW ERROR GOT HERE?, ", reviewIDs.length);
-		connection.query(
-			allReviewsQuery,
-			[inParkIDSet],
-			async (err, reviewsResults) => {
-				if (err) {
-					console.log("failed" + err);
-					res.sendStatus(500);
-					return;
-				}
-				//console.log("results is: ", reviewsResults);
-				var reviewsJSON = JSON.parse(JSON.stringify(reviewsResults));
-				console.log("reviews is: ", reviewsJSON);
-
-				for (var i = 0; i < parkDataJSON.length; i++) {
-					parkDataJSON[i].weather = {};
-					for (var x = 0; x < reviewsJSON.length; x++) {
-						if (reviewsJSON[x].p_id == parkDataJSON[i].id) {
-							console.log(
-								"found matching ID! ",
-								reviewsJSON[x].p_id
-							);
-							parkDataJSON[i].avgScore = reviewsJSON[x].avgScore;
-							parkDataJSON[i].numReviews =
-								reviewsJSON[x].numReviews;
+		try {
+			var reviewsResults = await new Promise((resolve, reject) => {
+				connection.query(
+					allReviewsQuery,
+					[inParkIDSet],
+					(err, result) => {
+						if (err) {
+							reject(err);
+						} else {
+							resolve(result);
 						}
 					}
-				}
-
-				//STEP 5: GET WEATHER FOR PARKS
-
-				//Dataset is array of park coordinates
-				var parkCoordinates = [];
-				for (var i = 0; i < parkDataJSON.length; i++) {
-					parkCoordinates[i] = [
-						parkDataJSON[i].lat,
-						parkDataJSON[i].lng,
-					];
-				}
-
-				//Define custom distance function to use,
-				//since coordinate distances are not cartesian distances
-				function kmeansDistance(p, q) {
-					return geolib.getDistance(
-						{ lat: p[0], lng: p[1] },
-						{ lat: q[0], lat: q[1] }
-					);
-				}
-
-				//Clustering code
-				var kmeans = new clustering.KMEANS();
-				// parameters: 3 - number of clusters
-				var clusterCount = Math.round(parkDataJSON.length / 5); //TODO: Find a better cluster number
-				if (clusterCount > 10) clusterCount = 10;
-				var clusters = kmeans.run(
-					parkCoordinates,
-					clusterCount,
-					kmeansDistance
 				);
+			});
+		} catch (err) {
+			console.log("failed" + err);
+			res.sendStatus(500);
+			return;
+		}
 
-				//Find coordinates at the center of each cluster
-				let clusterCentroids = [];
-				for (
-					var clusterNum = 0;
-					clusterNum < clusters.length;
-					clusterNum++
-				) {
-					//Add park coordinates to the cluster the park is in
-					let clusterCoordinates = [];
-					for (var i = 0; i < clusters[clusterNum].length; i++) {
-						clusterCoordinates.push({
+		//console.log("results is: ", reviewsResults);
+		var reviewsJSON = JSON.parse(JSON.stringify(reviewsResults));
+		console.log("reviews is: ", reviewsJSON);
+
+		for (var i = 0; i < parkDataJSON.length; i++) {
+			parkDataJSON[i].weather = {};
+			for (var x = 0; x < reviewsJSON.length; x++) {
+				if (reviewsJSON[x].p_id == parkDataJSON[i].id) {
+					console.log("found matching ID! ", reviewsJSON[x].p_id);
+					parkDataJSON[i].avgScore = reviewsJSON[x].avgScore;
+					parkDataJSON[i].numReviews = reviewsJSON[x].numReviews;
+				}
+			}
+		}
+
+		//STEP 5: GET WEATHER FOR PARKS
+
+		//Dataset is array of park coordinates
+		var parkCoordinates = [];
+		for (var i = 0; i < parkDataJSON.length; i++) {
+			parkCoordinates[i] = [parkDataJSON[i].lat, parkDataJSON[i].lng];
+		}
+
+		//Define custom distance function to use,
+		//since coordinate distances are not cartesian distances
+		function kmeansDistance(p, q) {
+			return geolib.getDistance(
+				{ lat: p[0], lng: p[1] },
+				{ lat: q[0], lat: q[1] }
+			);
+		}
+
+		//Clustering code
+		var kmeans = new clustering.KMEANS();
+		// parameters: 3 - number of clusters
+		var clusterCount = Math.round(parkDataJSON.length / 5); //TODO: Find a better cluster number
+		if (clusterCount > 10) clusterCount = 10;
+		var clusters = kmeans.run(
+			parkCoordinates,
+			clusterCount,
+			kmeansDistance
+		);
+
+		//Find coordinates at the center of each cluster
+		let clusterCentroids = [];
+		for (var clusterNum = 0; clusterNum < clusters.length; clusterNum++) {
+			//Add park coordinates to the cluster the park is in
+			let clusterCoordinates = [];
+			for (var i = 0; i < clusters[clusterNum].length; i++) {
+				clusterCoordinates.push({
+					lat: parkDataJSON[clusters[clusterNum][i]].lat,
+					lng: parkDataJSON[clusters[clusterNum][i]].lng,
+				});
+			}
+			//console.log(clusterCoordinates);
+			let clusterCentroid = geolib.getCenter(clusterCoordinates);
+			//console.log(clusterCentroid);
+
+			//Calculate each park's distance to cluster center
+			let clusterDist = [];
+			for (var i = 0; i < clusters[clusterNum].length; i++) {
+				parkDataJSON[clusters[clusterNum][i]].cluster = clusterNum;
+				clusterDist.push(
+					geolib.getDistance(
+						{
 							lat: parkDataJSON[clusters[clusterNum][i]].lat,
 							lng: parkDataJSON[clusters[clusterNum][i]].lng,
-						});
-					}
-					//console.log(clusterCoordinates);
-					let clusterCentroid = geolib.getCenter(clusterCoordinates);
-					//console.log(clusterCentroid);
+						},
+						clusterCentroid
+					)
+				);
+			}
 
-					//Calculate each park's distance to cluster center
-					let clusterDist = [];
-					for (var i = 0; i < clusters[clusterNum].length; i++) {
-						parkDataJSON[
-							clusters[clusterNum][i]
-						].cluster = clusterNum;
-						clusterDist.push(
+			console.log(
+				"Cluster",
+				clusterNum,
+				"...Max dist:",
+				Math.max.apply(null, clusterDist),
+				"...Avg dist:",
+				clusterDist.reduce((a, b) => a + b) / clusterDist.length
+			);
+
+			//TODO: CHECK IF CLUSTERDIST IS OKAY
+			clusterCentroids.push(clusterCentroid);
+		}
+		console.log(clusterCentroids);
+
+		//Grab weather for each cluster
+		let weatherInstance = null;
+		let response = null;
+		for (var clusterNum = 0; clusterNum < clusters.length; clusterNum++) {
+			var times = suncalc.getTimes(
+				new Date(utime),
+				clusterCentroids[clusterNum].latitude,
+				clusterCentroids[clusterNum].longitude
+			);
+			var nightTime = new Date(times.night);
+			var dawnTime = new Date(times.dawn);
+
+			//If current time is past night-time, or before dawn, use current weather
+			if (utime > nightTime || utime < dawnTime) {
+				weatherURL = `http://api.openweathermap.org/data/2.5/weather?lat=${clusterCentroids[clusterNum].latitude}&lon=${clusterCentroids[clusterNum].longitude}&appid=${weatherKey1}&units=metric`;
+
+				response = await axios
+					.get(weatherURL)
+					.then((response) => response.data)
+					.catch(false);
+
+				weatherInstance = response;
+
+				for (var i = 0; i < clusters[clusterNum].length; i++) {
+					parkDataJSON[clusters[clusterNum][i]].weather = {
+						time: utime.getTime(),
+						city: response.name,
+						clouds: weatherInstance.clouds.all,
+						cloudDesc: weatherInstance.weather[0].description,
+						humidity: weatherInstance.main.humidity,
+						temp: weatherInstance.main.temp,
+						stationCoord: {
+							lat: response.coord.lat,
+							lng: response.coord.lon,
+						},
+						stationDist:
 							geolib.getDistance(
 								{
 									lat:
@@ -1071,240 +1130,162 @@ app.post("/api/getParkData", async (req, res) => {
 										parkDataJSON[clusters[clusterNum][i]]
 											.lng,
 								},
-								clusterCentroid
-							)
-						);
-					}
-
-					console.log(
-						"Cluster",
-						clusterNum,
-						"...Max dist:",
-						Math.max.apply(null, clusterDist),
-						"...Avg dist:",
-						clusterDist.reduce((a, b) => a + b) / clusterDist.length
-					);
-
-					//TODO: CHECK IF CLUSTERDIST IS OKAY
-					clusterCentroids.push(clusterCentroid);
-				}
-				console.log(clusterCentroids);
-
-				//Grab weather for each cluster
-				let weatherInstance = null;
-				let response = null;
-				for (
-					var clusterNum = 0;
-					clusterNum < clusters.length;
-					clusterNum++
-				) {
-					var times = suncalc.getTimes(
-						new Date(utime),
-						clusterCentroids[clusterNum].latitude,
-						clusterCentroids[clusterNum].longitude
-					);
-					var nightTime = new Date(times.night);
-					var dawnTime = new Date(times.dawn);
-
-					//If current time is past night-time, or before dawn, use current weather
-					if (utime > nightTime || utime < dawnTime) {
-						weatherURL = `http://api.openweathermap.org/data/2.5/weather?lat=${clusterCentroids[clusterNum].latitude}&lon=${clusterCentroids[clusterNum].longitude}&appid=${weatherKey1}&units=metric`;
-
-						response = await axios
-							.get(weatherURL)
-							.then((response) => response.data)
-							.catch(false);
-
-						weatherInstance = response;
-
-						for (var i = 0; i < clusters[clusterNum].length; i++) {
-							parkDataJSON[clusters[clusterNum][i]].weather = {
-								time: utime.getTime(),
-								city: response.name,
-								clouds: weatherInstance.clouds.all,
-								cloudDesc:
-									weatherInstance.weather[0].description,
-								humidity: weatherInstance.main.humidity,
-								temp: weatherInstance.main.temp,
-								stationCoord: {
+								{
 									lat: response.coord.lat,
 									lng: response.coord.lon,
-								},
-								stationDist:
-									geolib.getDistance(
-										{
-											lat:
-												parkDataJSON[
-													clusters[clusterNum][i]
-												].lat,
-											lng:
-												parkDataJSON[
-													clusters[clusterNum][i]
-												].lng,
-										},
-										{
-											lat: response.coord.lat,
-											lng: response.coord.lon,
-										}
-									) / 1000,
-							};
-						}
-
-						//Otherwise use forecast weather
-					} else {
-						weatherURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${clusterCentroids[clusterNum].latitude}&lon=${clusterCentroids[clusterNum].longitude}&cnt=50&appid=${weatherKey1}&units=metric`;
-
-						console.log(weatherURL);
-
-						response = await axios
-							.get(weatherURL)
-							.then((response) => response.data)
-							.catch(false);
-
-						//Go through the forecasts and find the closest one to nightfall
-						for (var i = 0; i < response.cnt; i++) {
-							console.log(
-								new Date(response.list[i].dt_txt).getTime(),
-								nightTime.getTime(),
-								new Date(response.list[i].dt_txt).getTime() >
-									nightTime.getTime()
-							);
-							if (
-								new Date(response.list[i].dt_txt).getTime() >
-								nightTime.getTime()
-							) {
-								//If the date before nightfall is closer to nightfall than the one after, pick the closer one
-								let succInst = i;
-								if (
-									i > 0 &&
-									Math.abs(
-										new Date(response.list[i - 1].dt_txt) -
-											nightTime.getTime()
-									) <
-										Math.abs(
-											new Date(
-												response.list[i].dt_txt
-											).getTime() - nightTime.getTime()
-										)
-								) {
-									succInst = i - 1;
 								}
-								console.log(
-									"Success! Looking at ",
-									succInst,
-									":",
-									response.list[succInst]
-								);
-								weatherInstance = response.list[succInst];
-								break;
-							}
-						}
+							) / 1000,
+					};
+				}
 
-						//Create weather object
-						for (var i = 0; i < clusters[clusterNum].length; i++) {
-							parkDataJSON[clusters[clusterNum][i]].weather = {
-								time: new Date(
-									weatherInstance.dt_txt
-								).getTime(),
-								city: response.city.name,
-								clouds: weatherInstance.clouds.all,
-								cloudDesc:
-									weatherInstance.weather[0].description,
-								humidity: weatherInstance.main.humidity,
-								temp: weatherInstance.main.temp,
-								stationCoord: {
-									lat: response.city.coord.lat,
-									lng: response.city.coord.lon,
-								},
-								stationDist:
-									geolib.getDistance(
-										{
-											lat:
-												parkDataJSON[
-													clusters[clusterNum][i]
-												].lat,
-											lng:
-												parkDataJSON[
-													clusters[clusterNum][i]
-												].lng,
-										},
-										{
-											lat: response.city.coord.lat,
-											lng: response.city.coord.lon,
-										}
-									) / 1000,
-							};
+				//Otherwise use forecast weather
+			} else {
+				weatherURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${clusterCentroids[clusterNum].latitude}&lon=${clusterCentroids[clusterNum].longitude}&cnt=50&appid=${weatherKey1}&units=metric`;
+
+				console.log(weatherURL);
+
+				response = await axios
+					.get(weatherURL)
+					.then((response) => response.data)
+					.catch(false);
+
+				//Go through the forecasts and find the closest one to nightfall
+				for (var i = 0; i < response.cnt; i++) {
+					console.log(
+						new Date(response.list[i].dt_txt).getTime(),
+						nightTime.getTime(),
+						new Date(response.list[i].dt_txt).getTime() >
+							nightTime.getTime()
+					);
+					if (
+						new Date(response.list[i].dt_txt).getTime() >
+						nightTime.getTime()
+					) {
+						//If the date before nightfall is closer to nightfall than the one after, pick the closer one
+						let succInst = i;
+						if (
+							i > 0 &&
+							Math.abs(
+								new Date(response.list[i - 1].dt_txt) -
+									nightTime.getTime()
+							) <
+								Math.abs(
+									new Date(
+										response.list[i].dt_txt
+									).getTime() - nightTime.getTime()
+								)
+						) {
+							succInst = i - 1;
 						}
+						console.log(
+							"Success! Looking at ",
+							succInst,
+							":",
+							response.list[succInst]
+						);
+						weatherInstance = response.list[succInst];
+						break;
 					}
 				}
 
-				// console.log(weatherInstance);
-
-				//console.log(clusters);
-
-				var phaseInfo = getMoon(utime);
-
-				let moonPercent = phaseInfo.fraction;
-				var moonType;
-				if (
-					inRange(phaseInfo.phase, 0.9375, 1) ||
-					inRange(phaseInfo.phase, 0, 0.0625)
-				) {
-					moonType = "New Moon";
-				} else if (inRange(phaseInfo.phase, 0.0625, 0.1875)) {
-					moonType = "Waxing Crescent";
-				} else if (inRange(phaseInfo.phase, 0.1875, 0.3125)) {
-					moonType = "First Quarter";
-				} else if (inRange(phaseInfo.phase, 0.3125, 0.4375)) {
-					moonType = "Waxing Gibbous";
-				} else if (inRange(phaseInfo.phase, 0.4375, 0.5625)) {
-					moonType = "Full Moon";
-				} else if (inRange(phaseInfo.phase, 0.5625, 0.6875)) {
-					moonType = "Waning Gibbous";
-				} else if (inRange(phaseInfo.phase, 0.6875, 0.8125)) {
-					moonType = "Last Quarter";
-				} else if (inRange(phaseInfo.phase, 0.8125, 0.9375)) {
-					moonType = "Waning Crescent";
-				} else {
-					moonType = "New Moon";
+				//Create weather object
+				for (var i = 0; i < clusters[clusterNum].length; i++) {
+					parkDataJSON[clusters[clusterNum][i]].weather = {
+						time: new Date(weatherInstance.dt_txt).getTime(),
+						city: response.city.name,
+						clouds: weatherInstance.clouds.all,
+						cloudDesc: weatherInstance.weather[0].description,
+						humidity: weatherInstance.main.humidity,
+						temp: weatherInstance.main.temp,
+						stationCoord: {
+							lat: response.city.coord.lat,
+							lng: response.city.coord.lon,
+						},
+						stationDist:
+							geolib.getDistance(
+								{
+									lat:
+										parkDataJSON[clusters[clusterNum][i]]
+											.lat,
+									lng:
+										parkDataJSON[clusters[clusterNum][i]]
+											.lng,
+								},
+								{
+									lat: response.city.coord.lat,
+									lng: response.city.coord.lon,
+								}
+							) / 1000,
+					};
 				}
-
-				let sunTimeData = suncalc.getTimes(
-					utime,
-					parseFloat(lat),
-					parseFloat(lng)
-				);
-				let moonTimeData = suncalc.getMoonTimes(
-					utime,
-					parseFloat(lat),
-					parseFloat(lng)
-				);
-
-				//STEP 9: FORMAT RESPONSE JSON
-				let reply = {
-					totalResults: totalResults,
-					shownResults: shownResults,
-					parks: parkDataJSON,
-					moonFraction: phaseInfo.fraction,
-					moonPercent: phaseInfo.phase,
-					moonType: moonType,
-					stellarData: {
-						sunrise: sunTimeData.sunrise,
-						sunset: sunTimeData.sunset,
-						night: sunTimeData.night,
-						nightEnd: sunTimeData.nightEnd,
-						moonrise: moonTimeData.rise,
-						moonset: moonTimeData.set,
-					},
-				};
-				console.log("Response ", reply);
-
-				//STEP 10: SEND DATA TO FRONT-END
-				res.send(reply);
 			}
+		}
+
+		// console.log(weatherInstance);
+
+		//console.log(clusters);
+
+		var phaseInfo = getMoon(utime);
+
+		let moonPercent = phaseInfo.fraction;
+		var moonType;
+		if (
+			inRange(phaseInfo.phase, 0.9375, 1) ||
+			inRange(phaseInfo.phase, 0, 0.0625)
+		) {
+			moonType = "New Moon";
+		} else if (inRange(phaseInfo.phase, 0.0625, 0.1875)) {
+			moonType = "Waxing Crescent";
+		} else if (inRange(phaseInfo.phase, 0.1875, 0.3125)) {
+			moonType = "First Quarter";
+		} else if (inRange(phaseInfo.phase, 0.3125, 0.4375)) {
+			moonType = "Waxing Gibbous";
+		} else if (inRange(phaseInfo.phase, 0.4375, 0.5625)) {
+			moonType = "Full Moon";
+		} else if (inRange(phaseInfo.phase, 0.5625, 0.6875)) {
+			moonType = "Waning Gibbous";
+		} else if (inRange(phaseInfo.phase, 0.6875, 0.8125)) {
+			moonType = "Last Quarter";
+		} else if (inRange(phaseInfo.phase, 0.8125, 0.9375)) {
+			moonType = "Waning Crescent";
+		} else {
+			moonType = "New Moon";
+		}
+
+		let sunTimeData = suncalc.getTimes(
+			utime,
+			parseFloat(lat),
+			parseFloat(lng)
 		);
+		let moonTimeData = suncalc.getMoonTimes(
+			utime,
+			parseFloat(lat),
+			parseFloat(lng)
+		);
+
+		//STEP 9: FORMAT RESPONSE JSON
+		let reply = {
+			totalResults: totalResults,
+			shownResults: shownResults,
+			parks: parkDataJSON,
+			moonFraction: phaseInfo.fraction,
+			moonPercent: phaseInfo.phase,
+			moonType: moonType,
+			stellarData: {
+				sunrise: sunTimeData.sunrise,
+				sunset: sunTimeData.sunset,
+				night: sunTimeData.night,
+				nightEnd: sunTimeData.nightEnd,
+				moonrise: moonTimeData.rise,
+				moonset: moonTimeData.set,
+			},
+		};
+		console.log("Response ", reply);
+
+		//STEP 10: SEND DATA TO FRONT-END
+		res.send(reply);
 	} else {
-		res.sendStatus(204);
+		res.sendStatus(204); //Why does not having review IDs cause a 204 error?
 		return;
 	}
 });
